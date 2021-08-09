672afa1f11b681572857ad78e832cd9ef6a4c088
rio1004666 <rkdqudghk12@naver.com>
2021-08-09 16:54:36 +0900

    2021/08/09 파이썬Ch01

---
+++ Ch01/1_1_Hello.py
@@ -0,0 +1,14 @@
+"""
+날짜 : 2021 / 08 / 09
+이름 : 강병화
+내용 : 파이썬 개발환경 설정 교재 p15
+"""
+
+
+print('hello world')
+print('hello python!')
+
+#end옵션을 이용한 한줄 출력
+print('hello', end='! ')   # 줄바꿈을 없애주고 문자를 삽입할 수 있다
+print('hello', end='! ')
+print('korea')
